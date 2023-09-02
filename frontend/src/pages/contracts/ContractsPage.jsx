cda70b136a51eb6563eedc18feac37e9d09d8c4e
sayfelanjos <saymonfelipe@hotmail.com>
2023-09-02 00:34:03 -0300

    Add-Jenkins-pipeline

---
+++ frontend/src/pages/contracts/ContractsPage.jsx
@@ -0,0 +1,10 @@
+import React, { Suspense } from "react";
+
+const ContractsPage = () => {
+  return (
+    // TODO: need to put a fallback component
+    <></>
+  );
+};
+
+export default ContractsPage;
